7112352c820a4b640e39f6b2611bd771a7e585d8
mpaulweeks <mpaulweeks@gmail.com>
2023-12-09 13:09:32 -0500

    get 2nd page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <main className="flex flex-col justify-center items-middle min-h-screen text-center">
+      <img
+        className="mx-auto"
+        src="/lemon64x64.png"
+        alt="lemon"
+        width={64}
+        height={64}
+      />
+      <h1>
+        About
+      </h1>
+    </main>
+  )
+}
