bfdc154814c759ef13e8b72462427d88c2944583
mpaulweeks <mpaulweeks@gmail.com>
2023-12-09 16:21:24 -0500

    fixed + block navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,33 @@
 import Link from "next/link";
 
-export function NavBar() {
+export function DesktopNav() {
   return (
-    <main className="flex flex-row justify-center items-center bg-black text-white p-4 text-lg gap-4 font-bold">
+    <main className="flex-row justify-center items-center bg-black text-white p-4 text-lg gap-4 font-bold hidden md:flex">
       <img
         className="inline-block h-10"
         src="/lemon64x64.png"
       />
-      <Link href="/" className="flex-1">
-        M. Paul Games
-      </Link>
+      <div className="flex-1">
+        <Link href="/">
+          M. Paul Games
+        </Link>
+      </div>
       <Link href="/about">
         About
       </Link>
     </main>
+  )
+}
+
+export function NavBar() {
+  return (
+    <>
+      <nav className="fixed w-full top-0 z-10">
+        <DesktopNav />
+      </nav>
+      <nav className="block">
+        <DesktopNav />
+      </nav>
+    </>
   )
 }
