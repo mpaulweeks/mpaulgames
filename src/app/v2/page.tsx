274e5d31b85588a4ca8739701488b6002a0caa13
mpaulweeks <mpaulweeks@gmail.com>
2023-12-13 01:08:13 -0500

    change default routes

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,12 +1,10 @@
 import { projects } from "@/components/data";
 import { Feature } from "@/components/feature";
 
-export function HomeV2() {
+export default function HomeV2() {
   return (
     <main className="flex flex-col justify-center items-center text-center">
       {projects.map(p => <Feature {...p} key={['feature', p.title].join('-')} /> )}
     </main>
   )
 }
-
-export default HomeV2;
