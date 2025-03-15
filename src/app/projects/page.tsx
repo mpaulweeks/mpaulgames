a4c7a3a4875245d091266486494f6169e21accc1
mpaulweeks <mpaulweeks@gmail.com>
2025-03-15 14:36:40 -0400

    remove gap on mobile

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import { Feature } from '@/components/feature';
 
 export default function ProjectsPage() {
   return (
-    <main className="flex flex-col justify-center items-center text-center bg-white gap-2 md:gap-0">
+    <main className="flex flex-col justify-center items-center text-center bg-white gap-0">
       {projects.map(p => (
         <Feature {...p} key={['feature', p.title].join('-')} />
       ))}
